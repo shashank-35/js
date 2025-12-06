4bc1804d3379aade8d332544abd25587456f57c4
shashank kanade <kanadeshashank35@gmail.com>
2025-12-06 14:27:01 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,21 @@
+let arr = ["shashank","dhiraj","mohit","nilesh","ankit"];
+
+function print(arr, number)
+{
+    if(number > arr .length)
+        {
+    console.log("🚀 number is greater than array length");
+    }
+    else if(number < 0)
+    {
+    console.log("🚀 number is negative");
+    }else{
+    for(let i=0; i<number; i++)
+        {
+        console.log("🚀 ~ name:", arr[i])
+    }
+    }
+}
+
+
+print(arr, 3);
---
+++ variable.js
@@ -0,0 +1,52 @@
+// C:\Users\SHASHANK\AppData\Local\nvm
+// C:\nvm4w\nodejs
+/**
+ * 
+ * 
+ * nvm install <version> -> nvm install 20
+ * nvm use <version> -> nvm use 20
+ * nvm list / nvm ls -> list of all installed versions
+ * nvm -v -> version of nvm
+ * nvm uninstall <version> -> nvm uninstall 20
+ * node -v -> version of node 
+ */
+// var a = 10;
+// console.log("🚀 ~ a:", a)
+// console.log("🚀 ~ a:", a)
+
+// let a = 20;
+// switch (true) {
+//     case (a < 18):
+//         console.log("🚀 you are minor:", a)
+//         break;
+//     case (a > 18):
+//         console.log("🚀 you are adult:", a)
+//         break; 
+// }
+let a = 20;
+switch (a) {
+    case 1:
+        console.log("🚀 a is 1:", a)
+        break;
+    case 2:
+        console.log("🚀 a is 2:", a)
+        break;
+    case 3:
+        console.log("🚀 a is 3:", a)
+        break;
+    case 4:
+        console.log("🚀 a is 4:", a)
+        break;
+    case 5:
+        console.log("🚀 a is 5:", a)
+        break;
+    case 6:
+        console.log("🚀 a is 6:", a)
+        break;
+    case 20:
+        console.log("🚀 a is 20:", a)
+        break;
+    default:
+        console.log("🚀 a is not in between 1 to 6:", a)
+        break;
+}
