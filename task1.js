e397323ce52f7be3d99c84773da4924ee1bda133
shashank kanade <kanadeshashank35@gmail.com>
2025-12-08 10:29:10 +0530

    perform program using function and if else condition

--- task1.js
+++ task1.js
@@ -1,21 +1,15 @@
-let arr = ["shashank","dhiraj","mohit","nilesh","ankit"];
+let arr = ["shashank", "dhiraj", "mohit", "nilesh", "ankit"];
 
-function print(arr, number)
-{
-    if(number > arr .length)
-        {
-    console.log("🚀 number is greater than array length",number);
-    }
-    else if(number < 0)
-    {
+function print(arr, number) {
+  if (number > arr.length) {
+    console.log("🚀 number is greater than array length", number);
+  } else if (number < 0) {
     console.log("🚀 number is negative");
-    }else{
-    for(let i=0; i<number; i++)
-        {
-        console.log("🚀 ~ name:", arr[i])
+  } else {
+    for (let i = 0; i < number; i++) {
+      console.log("🚀 ~ name:", arr[i]);
     }
-    }
+  }
 }
-
 
 print(arr, 3);
