ee784ccf12453badce71172598e2749d555c3514
shashank kanade <kanadeshashank35@gmail.com>
2025-12-24 14:00:37 +0530

    build todo list and otp generator using js

--- exam practical/q2.js
+++ exam practical/q2.js
@@ -6,13 +6,15 @@
 
 // Baaki letters same rakho
 // 👉 Output: "Hello World"
-let str = "hello world";
-let str1 = str.split(" ");
-console.log("🚀 ~ str1:", str1)
+// let str = "hello world";
+// let str1 = str.split(" ");
+// console.log("🚀 ~ str1:", str1)
 
-for (let i = 0; i < str1.length; i++) {
-    str1[i] = str1[i].charAt(0).toUpperCase() + str1[i].slice(1);
-}
-console.log("🚀 ~ str1:", str1)
-let result = str1.join(" ");
-console.log("🚀 ~ result:", result)
+// for (let i = 0; i < str1.length; i++) {
+//     str1[i] = str1[i].charAt(0).toUpperCase() + str1[i].slice(1);
+// }
+// console.log("🚀 ~ str1:", str1)
+// let result = str1.join(" ");
+// console.log("🚀 ~ result:", result)
+
+
